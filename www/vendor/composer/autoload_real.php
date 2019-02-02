b50a1786503ba40775d4b6c8198758649b8fdb6a
Xorinzor <4212335+xorinzor@users.noreply.github.com>
2019-02-02 15:17:16 +0100

    travis config file change

--- www/vendor/composer/autoload_real.php
+++ www/vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit000d3c436348d83dcff02341bc5b7cc8
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit000d3c436348d83dcff02341bc5b7cc8', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
