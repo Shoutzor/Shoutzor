ad4adb0bf762713a3e0e23493582d062af3e64f5
xorinzor <4212335+xorinzor@users.noreply.github.com>
2020-08-09 16:54:46 +0200

    implement even more Job logic

--- app/Http/Controllers/Api/UploadApiController.php
+++ app/Http/Controllers/Api/UploadApiController.php
@@ -3,18 +3,45 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Jobs\ProcessUpload;
+use App\Upload;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UploadApiController extends Controller {
 
     public function store(Request $request) {
+        //Check if a file has been provided with the request
+        if($request->hasFile('media') !== true) {
+            return;
+        }
 
-        //@TODO create a Task for each file that's uploaded, to queue them for processing.
+        //Check if there are any errors with the file upload
+        if($request->file('media')->isValid() !== true) {
+            return;
+        }
+
+        //Get the name and extension of the file
+        $name   = $request->file('media')->getClientOriginalName();
+        $ext    = $request->file('media')->extension();
+
+        //Set the new destination and name for the file
+        $destination = 'temp/upload';
+        $newName     = time().$name.'.'.$ext;
+
         //Move the file to a temporary directory while it's awaiting processing.
+        $request->file('media')->storeAs($destination, $newName);
 
-        //$request->file('media')->extension();
-        //$request->file('media')->storeAs('where/to/store', 'filename.ext');
+        //Store the upload in the database for use in the Job
+        $upload = new Upload();
+        $upload->filename   = $newName;
+        //$upload->user_id    = $request->user()->id;
+        $upload->user_id    = 1;
+        $upload->status     = Upload::STATUS_QUEUED;
+        $upload->save();
 
+        //Queue the job for processing
+        ProcessUpload::dispatch($upload)->onQueue('uploads');
     }
 
 }
