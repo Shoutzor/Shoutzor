6a47686945763ef76c9ad5ba3284646e49213a61
xorinzor <4212335+xorinzor@users.noreply.github.com>
2020-08-09 10:01:50 +0200

    Add process class

---
+++ app/Jobs/ProcessUpload.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ProcessUpload implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $file;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param  Upload  $podcast
+     * @return void
+     */
+    public function __construct(Upload $file)
+    {
+        $this->file = $file;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @param  MediaProcessor  $processor
+     * @return void
+     */
+    public function handle(MediaProcessor $processor)
+    {
+        // Process uploaded podcast...
+    }
+
+    /**
+     * Handle a job failure.
+     *
+     * @param  \Exception  $exception
+     * @return void
+     */
+    public function failed(Exception $exception)
+    {
+        // Send user notification of failure, etc...
+    }
+
+    /**
+     * Determine the time at which the job should timeout.
+     *
+     * @return \DateTime
+     */
+    public function retryUntil()
+    {
+        return now()->addMinutes(5);
+    }
+}
