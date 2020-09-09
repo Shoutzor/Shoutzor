9f13cc4a7cddd5e2c40ec6e1feb619632b782cce
xorinzor <4212335+xorinzor@users.noreply.github.com>
2020-09-09 23:23:15 +0200

    cleanup unused directories

--- Modules/ShoutzorAcoustId/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('shoutzoracoustid::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('shoutzoracoustid.name') !!}
-    </p>
-@endsection
