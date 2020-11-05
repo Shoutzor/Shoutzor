249682ead4bea38d6b43ec5a296e54b618235426
xorinzor <4212335+xorinzor@users.noreply.github.com>
2020-11-05 23:01:20 +0100

    Added visual feedback when resuming session

--- resources/js/components/login/LoginScreen.vue
+++ resources/js/components/login/LoginScreen.vue
@@ -4,12 +4,16 @@
             <div class="text-center mb-4">
                 <img src="@static/images/shoutzor-logo-header.png" id="logo" alt="Shoutz0r logo">
             </div>
-            <form class="card card-md" action="#" @submit.prevent="login" method="post">
+            <div class="card card-md">
                 <div class="card-body">
                     <h2 class="mb-3 text-center">Login to your account</h2>
-                    <login-form></login-form>
+                    <login-form v-if="hasToken === false"></login-form>
+                    <div v-if="hasToken === true" class="text-center">
+                        <p>Loading user information</p>
+                        <div class="spinner-border" role="status"></div>
+                    </div>
                 </div>
-            </form>
+            </div>
             <div class="text-center text-muted">
                 Don't have account yet? <a href="#" tabindex="-1">Sign up</a>
             </div>
@@ -19,10 +23,14 @@
 
 <script>
 import LoginForm from "@js/components/login/LoginForm";
+import {mapGetters} from "vuex";
 
 export default {
     name: 'LoginScreen',
-    components: {LoginForm}
+    components: {LoginForm},
+    computed: mapGetters({
+        hasToken: 'hasToken'
+    }),
 }
 </script>
 
