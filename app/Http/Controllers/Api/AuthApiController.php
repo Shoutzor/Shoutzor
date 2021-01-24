c1da622a617bd57095d8771290138d5cb55ba1ca
xorinzor <4212335+xorinzor@users.noreply.github.com>
2021-01-24 22:27:46 +0100

    remove unused import

--- app/Http/Controllers/Api/AuthApiController.php
+++ app/Http/Controllers/Api/AuthApiController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
 use App\User;
-use Illuminate\Support\Facades\Hash;
 
 class AuthApiController extends Controller
 {
