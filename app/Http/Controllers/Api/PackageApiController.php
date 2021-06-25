8bcc2ceeb02f78838c33567b4eb1a2650b7db677
xorinzor <4212335+xorinzor@users.noreply.github.com>
2021-06-25 22:55:13 +0200

    replaced my own package-sysem with caffeinated modules

--- app/Http/Controllers/Api/PackageApiController.php
+++ app/Http/Controllers/Api/PackageApiController.php
@@ -10,41 +10,16 @@ use Illuminate\Http\Request;
 
 class PackageApiController extends Controller
 {
-
-    private PackageManager $pm;
-
     public function __construct()
     {
-        $this->pm = app(PackageManager::class);
+        //$this->pm = app(PackageManager::class);
     }
 
     public function installed(Request $request)
     {
         $request->validate(['id' => 'string']);
 
-        //@todo this can be removed once the package marketplace is implemented
-        //At which point, manual installation of packages via the filesystem is no longer supported
-        //(but can still be manually installed via the admin UI)
-        $this->pm->updateClassmap();
-
-        $packages = $this->pm->fetchPackages();
-        $result = [];
-
-        if ($request->id !== null) {
-            foreach ($packages as $pkg) {
-                if ($request->id === $pkg->getId()) {
-                    $result[] = new Package($pkg);
-                } else {
-                    return response()->json(['message' => 'Package with id '.$request->id.' not found'], 404);
-                }
-            }
-        } else {
-            foreach ($packages as $pkg) {
-                $result[] = new Package($pkg);
-            }
-        }
-
-        return response()->json($result, 200);
+        return response()->json([], 200);
     }
 
     /**
@@ -56,24 +31,6 @@ class PackageApiController extends Controller
     public function enable(Request $request)
     {
         $request->validate(['id' => 'string']);
-
-        //@todo this can be removed once the package marketplace is implemented
-        //At which point, manual installation of packages via the filesystem is no longer supported
-        //(but can still be manually installed via the admin UI)
-        $this->pm->updateClassmap();
-
-        //Find the package via it's ID
-        $pkg = $this->pm->findPackageById($request->id);
-
-        //Check if the package was found
-        if (is_null($pkg) === false) {
-            //Enable the package
-            $this->pm->enablePackage($pkg);
-            $this->pm->updateEnabledPackagesList();
-
-            //Return the success response
-            return response()->json(new Package($pkg), 200);
-        }
 
         //Return the failed response
         return response()->json(['message' => 'Package not found'], 400);
@@ -88,24 +45,6 @@ class PackageApiController extends Controller
     public function disable(Request $request)
     {
         $request->validate(['id' => 'string']);
-
-        //@todo this can be removed once the package marketplace is implemented
-        //At which point, manual installation of packages via the filesystem is no longer supported
-        //(but can still be manually installed via the admin UI)
-        $this->pm->updateClassmap();
-
-        //Find the package via it's ID
-        $pkg = $this->pm->findPackageById($request->id);
-
-        //Check if the package was found
-        if (is_null($pkg) === false) {
-            //Disable the package
-            $this->pm->disablePackage($pkg);
-            $this->pm->updateEnabledPackagesList();
-
-            //Return the success response
-            return response()->json(new Package($pkg), 200);
-        }
 
         //Return the failed response
         return response()->json(['message' => 'Package not found'], 400);
