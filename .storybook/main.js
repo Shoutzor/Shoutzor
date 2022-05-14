6076bb65f890a364d34784e892e44a4e99bbebcb
xorinzor <4212335+xorinzor@users.noreply.github.com>
2022-05-14 11:13:59 +0200

    added storybook gitignore

--- .storybook/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  "stories": ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials"],
-  "framework": "@storybook/vue3",
-  core: {
-    builder: "webpack5"
-  }
-};
