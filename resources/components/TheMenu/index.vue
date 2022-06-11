de582c36682858c3f47eaf30ff58c830676f0b53
xorinzor <4212335+xorinzor@users.noreply.github.com>
2022-06-11 11:14:47 +0200

    Changed base layout to make use of flexbox

--- resources/components/TheMenu/index.vue
+++ resources/components/TheMenu/index.vue
@@ -1,50 +1,36 @@
 <template>
     <nav id="navbar-left" class="fixed d-lg-block sidebar collapse">
-        <div class="position-sticky">
-            <div id="navbar-left-menu">
-                <span class="sidebar-heading d-flex justify-content-between align-items-center px-3 mb-1 text-muted">Music zone</span>
-                <ul class="nav flex-column">
-                    <li class="nav-item">
-                        <router-link
-                            :to="{name: 'dashboard'}"
-                            class="nav-link"
-                        >
-                            <b-icon-house-fill></b-icon-house-fill>
-                            Dashboard
-                        </router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link
-                            :to="{name: 'dashboard'}"
-                            class="nav-link"
-                        >
-                            <b-icon-clock-history></b-icon-clock-history>
-                            History
-                        </router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link
-                            :to="{name: 'dashboard'}"
-                            class="nav-link"
-                        >
-                            <b-icon-star></b-icon-star>
-                            Most Played
-                        </router-link>
-                    </li>
-                </ul>
-                <span v-if="isAuthenticated" class="sidebar-heading d-flex justify-content-between align-items-center px-3 mb-1 text-muted">Your zone</span>
-                <ul v-if="isAuthenticated" class="nav flex-column">
-                    <li class="nav-item">
-                        <router-link
-                            :to="{name: 'dashboard'}"
-                            class="nav-link"
-                        >
-                            <b-icon-cloud-upload></b-icon-cloud-upload>
-                            Upload manager
-                        </router-link>
-                    </li>
-                </ul>
-            </div>
+        <div id="navbar-left-menu">
+            <span class="sidebar-heading d-flex justify-content-between align-items-center px-3 mb-1 text-muted">Music zone</span>
+            <ul class="nav flex-column">
+                <li class="nav-item">
+                    <router-link :to="{name: 'dashboard'}" class="nav-link">
+                        <b-icon-house-fill></b-icon-house-fill>
+                        Dashboard
+                    </router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link :to="{name: 'dashboard'}" class="nav-link">
+                        <b-icon-clock-history></b-icon-clock-history>
+                        History
+                    </router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link :to="{name: 'dashboard'}" class="nav-link">
+                        <b-icon-star></b-icon-star>
+                        Most Played
+                    </router-link>
+                </li>
+            </ul>
+            <span v-if="isAuthenticated" class="sidebar-heading d-flex justify-content-between align-items-center px-3 mb-1 text-muted">Your zone</span>
+            <ul v-if="isAuthenticated" class="nav flex-column">
+                <li class="nav-item">
+                    <router-link :to="{name: 'dashboard'}" class="nav-link">
+                        <b-icon-cloud-upload></b-icon-cloud-upload>
+                        Upload manager
+                    </router-link>
+                </li>
+            </ul>
         </div>
     </nav>
 </template>
--- resources/js/views/Shoutzor.vue
+++ resources/js/views/Shoutzor.vue
@@ -1,22 +1,20 @@
 <template>
     <the-header id="header" :isAuthenticated="false" />
 
-    <div id="wrapper" class="container-fluid">
-        <div class="row">
-          <the-menu :isAuthenticated="false" />
+    <div id="wrapper" class="d-flex flex-nowrap">
+      <the-menu :isAuthenticated="false" class="d-flex flex-column flex-shrink-0 p-3" />
 
-          <main id="main-content" class="col">
-              <perfect-scrollbar ref="scroll">
-                  <div class="page">
-                      <div class="content">
-                          <div class="container-fluid">
-                              <router-view />
-                          </div>
+      <main id="main-content" class="d-flex flex-column flex-grow-1">
+          <perfect-scrollbar ref="scroll">
+              <div class="page">
+                  <div class="content">
+                      <div class="container-fluid">
+                          <router-view />
                       </div>
                   </div>
-              </perfect-scrollbar>
-          </main>
-        </div>
+              </div>
+          </perfect-scrollbar>
+      </main>
     </div>
 
     <media-player id="media-player" :volume="100" :playerStatus="'stopped'"/>
@@ -38,7 +36,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+#wrapper {
+    margin-top: $navbar-height;
+}
+
 #wrapper main {
     background: url('@static/images/main-content-noise-bg.png');
+
+    .ps {
+        flex: 1 1 auto;
+        height: 0; // Required to make content scrollable, see: https://stackoverflow.com/a/14964944/1024322
+    }
 }
 </style>
