01379108ef4e66ba5d059ccda199c3f2b3771aa2
xorinzor <4212335+xorinzor@users.noreply.github.com>
2022-06-18 16:42:33 +0200

    fixed fallback directive + removed credit due to plagiarism (I found the same code at multiple places)

--- resources/js/directives/mediaImageFallback.js
+++ resources/js/directives/mediaImageFallback.js
@@ -1,33 +1,21 @@
 import { defaultMediaImage } from "@js/config";
 
-
-/**
- * Credits to Arun Sivan: https://medium.com/@arunsivan18.8/vue-js-fallback-image-directive-4609a09afe4f
- */
 export default {
-    created(el, binding) {
+    created(el) {
         try {
-            const { value } = binding;
             const fallBackImage = defaultMediaImage;
             const img = new Image();
-            let error = fallBackImage;
             let original = el.src;
-            if (typeof value === 'string') {
-                error = value;
-            }
-            if (value instanceof Object) {
-                error = value.fallBackImage || fallBackImage;
-            }
             img.src = original;
             el.src = original;
             img.onload = () => {
                 el.src = original;
             };
-            img.onerror = () => {
-                el.src = error;
+            img.onerror = (e) => {
+                el.src = fallBackImage;
             };
         } catch (e) {
-            console.log(e)
+            console.log("fallback image failed", e);
         }
     }
 }
