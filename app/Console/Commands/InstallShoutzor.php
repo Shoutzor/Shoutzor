7328e28b8b4485d31c7d96a5efe3fa14e4896fb1
xorinzor <4212335+xorinzor@users.noreply.github.com>
2022-06-27 13:07:15 +0200

    refactored InstallShoutzor to make method less complex

--- app/Console/Commands/InstallShoutzor.php
+++ app/Console/Commands/InstallShoutzor.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Exceptions\ShoutzorInstallerException;
 use App\Exceptions\formValidationException;
 use App\HealthCheck\HealthCheckManager;
 use App\Installer\Installer;
@@ -135,114 +136,14 @@ class InstallShoutzor extends Command
         if ($useEnv) {
             // Inform the user about the --useenv option being used.
             $this->info('--useenv is used, existing .env file will be used.');
-
-            // Check if the .env file actually exists
-            if (file_exists(base_path('.env')) === false) {
-                throw new Exception('.env file not found in application root! Exiting.');
-            }
-
-            // Rebuild config cache
-            $step = $this->installer->rebuildConfigCache();
-
-            // Check if rebuilding the config cache worked
-            if ($step->succeeded() === false) {
-                throw new Exception('Failed to rebuild the config cache, reason: ' . $step->getOutput());
-            }
+            $this->loadEnvFile();
         }
 
         //Fetch the valid DB settings
         $dbFields = Installer::$dbFields;
 
-        // if --useenv is in-use, fetch the values from the .env file, otherwise, ask the user.
-        if ($useEnv) {
-            $this->info("Configuring SQL settings using the config values in the environment file");
-
-            $dbtype = config('database.default');
-            $host = config($dbFields[$dbtype]['host']['dotconfig']);
-            $port = config($dbFields[$dbtype]['port']['dotconfig']);
-
-            $this->line("Testing connection to $host:$port...");
-
-            if (!$socket = @fsockopen($host, $port, $errno, $errstr, 3)) {
-                throw new Exception("Could not connect to $host:$port; please ensure the correct hostname/IP address & port are used, and not being blocked by a firewall.");
-            } else {
-                $this->info("Connection success!");
-                fclose($socket);
-            }
-
-            $database = config($dbFields[$dbtype]['database']['dotconfig']);
-            $username = config($dbFields[$dbtype]['username']['dotconfig']);
-            $password = config($dbFields[$dbtype]['password']['dotconfig']);
-
-            $step = $this->installer->configureSql($dbtype, $host, $port, $database, $username, $password);
-
-            // Check if SQL configuration succeeded, if not, exit with error.
-            if ($step->succeeded()) {
-                $this->info("SQL Configuration succeeded");
-            } else {
-                // Check if it's a formValidation exception, or regular exception
-                if ($step->getException() instanceof formValidationException) {
-                    // $errors will now contain formValidationFieldError[] from the exception
-                    $errors = $step->getException()->getErrors();
-
-                    // Convert the array of formValidationFieldError objects into an array
-                    foreach ($errors as $e) {
-                        $this->error($e->getField() . ": " . $e->getMessage());
-                    }
-
-                    throw new Exception('SQL Configuration failed, reason: Validation failed');
-                } else {
-                    throw new Exception('SQL Configuration failed, reason: ' . $step->getOutput());
-                }
-            }
-        } else {
-            $this->info("Configuring SQL settings");
-
-            // Create a loop, this way the user can keep trying if the configuration fails.
-            while (true) {
-                // Ask the user what database type we should be configuring
-                $dbtype = $this->choice('Enter the sql type', array_keys($dbFields), 0);
-                $host = $this->anticipate('Enter the hostname of the sql server [ie: localhost or 127.0.0.1]', ['localhost', '127.0.0.1']);
-                $port = $this->anticipate('Enter the port of the sql server [ie: ' . $dbFields[$dbtype]['port']['default'] . ']', [$dbFields[$dbtype]['port']['default']]);
-
-                $this->line("Testing connection to $host:$port...");
-
-                if (!$socket = @fsockopen($host, $port, $errno, $errstr, 3)) {
-                    $this->error("Could not connect to $host:$port; please ensure the correct hostname/IP address & port are used, and not being blocked by a firewall.");
-                    fclose($socket);
-                    continue;
-                } else {
-                    $this->info("Connection success!");
-                    fclose($socket);
-                }
-
-                $database = $this->anticipate('Enter the name of the database [ie: shoutzor]', ['shoutzor']);
-                $username = $this->anticipate('Enter the username of the SQL account [ie: shoutzor]', ['shoutzor']);
-                $password = $this->secret('Enter the password of the SQL account');
-
-                $step = $this->installer->configureSql($dbtype, $host, $port, $database, $username, $password);
-
-                //Check if the SQL configuration succeeded, if so: break the loop
-                if ($step->succeeded()) {
-                    $this->info("SQL Configuration succeeded");
-                    break;
-                } else {
-                    // Check if it's a formValidation exception, or regular exception
-                    if ($step->getException() instanceof formValidationException) {
-                        // $errors will now contain formValidationFieldError[] from the exception
-                        $errors = $step->getException()->getErrors();
-
-                        // Convert the array of formValidationFieldError objects into an array
-                        foreach ($errors as $e) {
-                            $this->error($e->getField() . ": " . $e->getMessage());
-                        }
-                    } else {
-                        // Configuration failed, display error and restart the loop
-                        $this->error("SQL Configuration failed, reason: " . $step->getOutput());
-                    }
-                }
-            }
-        }
+        // Configure the database connection
+        $this->configureDbConnection($useEnv, $dbFields);
 
         // Retrieve the installation steps from the installer
         $installationSteps = Installer::$installSteps;
@@ -254,7 +155,7 @@ class InstallShoutzor extends Command
             $stepResult = $this->installer->{$step['method']}();
 
             if ($stepResult->succeeded() === false) {
-                throw new Exception("Installation step failed. Reason: " . $stepResult->getOutput());
+                throw new ShoutzorInstallerException("Installation step failed. Reason: " . $stepResult->getOutput());
             }
         }
 
@@ -262,10 +163,113 @@ class InstallShoutzor extends Command
             $this->info('Seeding database with the DevelopmentSeeder');
             $this->installer->developmentSeedDatabase();
             if ($stepResult->succeeded() === false) {
-                throw new Exception("Installation step failed. Reason: " . $stepResult->getOutput());
+                throw new ShoutzorInstallerException("Installation step failed. Reason: " . $stepResult->getOutput());
             }
         }
 
         $this->info('Installation finished!');
+    }
+
+    private function loadEnvFile() {
+        // Check if the .env file actually exists
+        if (file_exists(base_path('.env')) === false) {
+            throw new ShoutzorInstallerException('.env file not found in application root! Exiting.');
+        }
+
+        // Rebuild config cache
+        $step = $this->installer->rebuildConfigCache();
+
+        // Check if rebuilding the config cache worked
+        if ($step->succeeded() === false) {
+            throw new ShoutzorInstallerException('Failed to rebuild the config cache, reason: ' . $step->getOutput());
+        }
+    }
+
+    private function configureDbConnection($useEnv, $dbFields) {
+        // if --useenv is in-use, fetch the values from the .env file, otherwise, ask the user.
+        if ($useEnv) {
+            $this->info("Configuring SQL settings using the config values in the environment file");
+
+            $dbtype = config('database.default');
+            $host = config($dbFields[$dbtype]['host']['dotconfig']);
+            $port = config($dbFields[$dbtype]['port']['dotconfig']);
+            $database = config($dbFields[$dbtype]['database']['dotconfig']);
+            $username = config($dbFields[$dbtype]['username']['dotconfig']);
+            $password = config($dbFields[$dbtype]['password']['dotconfig']);
+
+            $this->testDbHostConnection($host, $port);
+            $this->testDbLogin($dbtype, $host, $port, $database, $username, $password);
+        }
+        else {
+            $this->info("Configuring SQL settings");
+
+            // Create a loop, this way the user can keep trying if the configuration fails.
+            while (true) {
+                // Ask the user what database type we should be configuring
+                $dbtype = $this->choice('Enter the sql type', array_keys($dbFields), 0);
+                $host = $this->anticipate('Enter the hostname of the sql server [ie: localhost or 127.0.0.1]', ['localhost', '127.0.0.1']);
+                $port = $this->anticipate('Enter the port of the sql server [ie: ' . $dbFields[$dbtype]['port']['default'] . ']', [$dbFields[$dbtype]['port']['default']]);
+
+                if($this->testDbHostConnection($host, $port)) {
+                    break;
+                }
+
+                $this->info("Please try again, or press CTRL + C to exit");
+            }
+
+            while(true) {
+                $database = $this->anticipate('Enter the name of the database [ie: shoutzor]', ['shoutzor']);
+                $username = $this->anticipate('Enter the username of the SQL account [ie: shoutzor]', ['shoutzor']);
+                $password = $this->secret('Enter the password of the SQL account');
+
+                if($this->testDbLogin($dbtype, $host, $port, $database, $username, $password)) {
+                    break;
+                }
+
+                $this->info("Please try again, or press CTRL + C to exit");
+            }
+        }
+    }
+
+    private function testDbHostConnection($host, $port)
+    {
+        $this->line("Testing connection to $host:$port...");
+
+        if (!$socket = @fsockopen($host, $port, $errno, $errstr, 3)) {
+            $this->error("Could not connect to $host:$port; please ensure the correct hostname/IP address & port are used, and not being blocked by a firewall.");
+            fclose($socket);
+            return false;
+        } else {
+            $this->info("Connection success!");
+            fclose($socket);
+            return true;
+        }
+    }
+
+    private function testDbLogin($dbtype, $host, $port, $database, $username, $password)
+    {
+        $step = $this->installer->configureSql($dbtype, $host, $port, $database, $username, $password);
+
+        //Check if the SQL configuration succeeded, if so: break the loop
+        if ($step->succeeded()) {
+            $this->info("SQL Configuration succeeded");
+            return true;
+        } else {
+            // Check if it's a formValidation exception, or regular exception
+            if ($step->getException() instanceof formValidationException) {
+                // $errors will now contain formValidationFieldError[] from the exception
+                $errors = $step->getException()->getErrors();
+
+                // Convert the array of formValidationFieldError objects into an array
+                foreach ($errors as $e) {
+                    $this->error($e->getField() . ": " . $e->getMessage());
+                }
+            } else {
+                // Configuration failed, display error and restart the loop
+                $this->error("SQL Configuration failed, reason: " . $step->getOutput());
+            }
+
+            return false;
+        }
     }
 }
