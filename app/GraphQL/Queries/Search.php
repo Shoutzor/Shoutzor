cabc2c196eda60d5d034f493e9d7974992f17d06
xorinzor <4212335+xorinzor@users.noreply.github.com>
2022-06-28 13:58:04 +0200

    created some abominable queries for Search

--- app/GraphQL/Queries/Search.php
+++ app/GraphQL/Queries/Search.php
@@ -14,35 +14,65 @@ final class Search
      */
     public function __invoke($_, array $args)
     {
-        $q = '%' . $args['q'] . '%';
+        $q = $args['q'];
+
+        // TODO query input sanitization
 
         $media = Media::query()
-            ->with('artists')
-            ->leftJoin('artist_media', function($join) {
-                $join->on('artist_media.media_id', '=', 'media.id');
-            })
-            ->leftJoin('artists', function($join) {
-                $join->on('artists.id', '=', 'artist_media.artist_id');
-            })
-            ->selectRaw('SUM( (MATCH (media.title) AGAINST (?) * 2) + MATCH (artists.name) AGAINST (?) ) AS relevance', [$q, $q])
+            ->with(['artists'])
+            ->leftJoin('artist_media', 'artist_media.media_id', '=', 'media.id')
+            ->leftJoin('artists', 'artists.id', '=', 'artist_media.artist_id')
+            ->leftJoin('albums', 'media.album_id', '=', 'albums.id')
+            ->selectRaw('SUM(
+                (MATCH (media.title) AGAINST (?) * 3) +
+                (MATCH (artists.name) AGAINST (?) * 2) +
+                MATCH (albums.title) AGAINST (?)
+            ) AS relevance', [$q, $q, $q])
             ->addSelect('artists.id as artist_id')
             ->addSelect('media.*')
             ->addSelect('media.id as id')
-            ->whereRaw('media.title LIKE ?', [$q])
-            ->orWhereRaw('artists.name LIKE ?', [$q])
             // TODO: find out if you can partial match-against ? ie: WHERE x LIKE '%something%'
-            //->whereRaw('MATCH (media.title) AGAINST (? IN BOOLEAN MODE)', [$q])
-            //->orWhereRaw('MATCH (artists.name) AGAINST (? IN BOOLEAN MODE)', [$q])
+            ->whereRaw('MATCH (media.title) AGAINST (? IN BOOLEAN MODE)', [$q])
+            ->orWhereRaw('MATCH (artists.name) AGAINST (? IN BOOLEAN MODE)', [$q])
+            ->orWhereRaw('MATCH (albums.title) AGAINST (? IN BOOLEAN MODE)', [$q])
             ->groupBy('media.id')
             ->orderBy('relevance', 'DESC')
             ->get();
 
         $artists = Artist::query()
-            ->where('name', 'like', $q)
+            ->leftJoin('artist_media', 'artist_media.artist_id', '=', 'artists.id')
+            ->leftJoin('media', 'media.id', '=', 'artist_media.media_id')
+            ->leftJoin('albums', 'media.album_id', '=', 'albums.id')
+            ->selectRaw('SUM(
+                (MATCH (artists.name) AGAINST (?) * 3) +
+                (MATCH (media.title) AGAINST (?) * 2) +
+                MATCH (albums.title) AGAINST (?)
+            ) AS relevance', [$q, $q, $q])
+            ->addSelect('artists.*')
+            // TODO: find out if you can partial match-against ? ie: WHERE x LIKE '%something%'
+            ->whereRaw('MATCH (media.title) AGAINST (? IN BOOLEAN MODE)', [$q])
+            ->orWhereRaw('MATCH (artists.name) AGAINST (? IN BOOLEAN MODE)', [$q])
+            ->orWhereRaw('MATCH (albums.title) AGAINST (? IN BOOLEAN MODE)', [$q])
+            ->groupBy('artists.id')
+            ->orderBy('relevance', 'DESC')
             ->get();
 
         $albums = Album::query()
-            ->where('title', 'like', $q)
+            ->leftJoin('media', 'albums.id', '=', 'media.album_id')
+            ->leftJoin('artist_media', 'artist_media.media_id', '=', 'media.id')
+            ->leftJoin('artists', 'artist_media.artist_id', '=', 'artists.id')
+            ->selectRaw('SUM(
+                (MATCH (albums.title) AGAINST (?) * 3) +
+                (MATCH (media.title) AGAINST (?) * 2) +
+                MATCH (artists.name) AGAINST (?)
+            ) AS relevance', [$q, $q, $q])
+            ->addSelect('albums.*')
+            // TODO: find out if you can partial match-against ? ie: WHERE x LIKE '%something%'
+            ->whereRaw('MATCH (media.title) AGAINST (? IN BOOLEAN MODE)', [$q])
+            ->orWhereRaw('MATCH (artists.name) AGAINST (? IN BOOLEAN MODE)', [$q])
+            ->orWhereRaw('MATCH (albums.title) AGAINST (? IN BOOLEAN MODE)', [$q])
+            ->groupBy('albums.id')
+            ->orderBy('relevance', 'DESC')
             ->get();
 
         return [
