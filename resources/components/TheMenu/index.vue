03af39068813701ac8f61d36f5b42ec596f69fe1
xorinzor <4212335+xorinzor@users.noreply.github.com>
2022-09-25 19:04:06 +0200

    Made left-hand menu content scrollable

--- resources/components/TheMenu/index.vue
+++ resources/components/TheMenu/index.vue
@@ -1,27 +1,29 @@
 <template>
-    <nav id="navbar-left" class="d-md-block col-12 col-md-4 col-lg-2 sidebar collapse">
-        <div id="navbar-left-menu" class="position-sticky">
-            <template v-for="(section, index) in items">
-                <span
-                    class="sidebar-heading d-flex justify-content-between align-items-center px-3 mb-1 text-muted"
-                    :class="index > 0 ? 'mt-md-5' : ''"
-                >
-                    {{ section.name }}
-                </span>
-                <ul class="nav flex-column">
-                    <template v-for="item in section.items">
-                        <template v-if="canAccess(item.route)">
-                            <li class="nav-item">
-                                <router-link :to="{name: item.route}" class="nav-link">
-                                    <component :is="item.icon" class="icon" />
-                                    <span class="text ps-1">{{ item.name }}</span>
-                                </router-link>
-                            </li>
+    <nav id="navbar-left" class="col-12 col-md-4 col-lg-2 sidebar collapse">
+        <perfect-scrollbar ref="menuScroll">
+            <div id="navbar-left-menu" class="position-sticky">
+                <template v-for="(section, index) in items">
+                    <span
+                        class="sidebar-heading d-flex justify-content-between align-items-center px-3 mb-1 text-muted"
+                        :class="index > 0 ? 'mt-md-5' : ''"
+                    >
+                        {{ section.name }}
+                    </span>
+                    <ul class="nav flex-column">
+                        <template v-for="item in section.items">
+                            <template v-if="canAccess(item.route)">
+                                <li class="nav-item">
+                                    <router-link :to="{name: item.route}" class="nav-link">
+                                        <component :is="item.icon" class="icon" />
+                                        <span class="text ps-1">{{ item.name }}</span>
+                                    </router-link>
+                                </li>
+                            </template>
                         </template>
-                    </template>
-                </ul>
-            </template>
-        </div>
+                    </ul>
+                </template>
+            </div>
+        </perfect-scrollbar>
     </nav>
 </template>
 
@@ -40,6 +42,12 @@ export default {
         return {
             routesObj: this.$router.getRoutes()
         }
+    },
+    mounted() {
+        this.updateScrollbar();
+    },
+    updated() {
+        this.updateScrollbar();
     },
     methods: {
         canAccess(routeName) {
@@ -61,6 +69,9 @@ export default {
         },
         getRoute(routeName) {
             return this.routesObj.find(x => x.name === routeName) || null;
+        },
+        updateScrollbar() {
+            this.$refs.menuScroll.update();
         }
     },
     computed: {
